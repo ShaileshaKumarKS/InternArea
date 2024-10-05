b9867a69b0729120e44b9c2563c13bb345920d6f
ShaileshaKumarKS <shailesh4ks@gmail.com>
2024-10-05 18:48:05 +0530

    Added email invoice

--- Routes/Payment.js
+++ Routes/Payment.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Use Stripe secret key
+const nodemailer=require('nodemailer');
 
 router.post('/', async (req, res) => {
     const { paymentMethodId, plan, email } = req.body;
@@ -24,28 +25,28 @@ router.post('/', async (req, res) => {
             expand: ['latest_invoice.payment_intent'],
         });
 
-        const invoiceId = subscription.latest_invoice;
-        const invoice=await stripe.invoice.retrieve(invoiceId);
+        // const invoiceId = subscription.latest_invoice;
+        // const invoice=await stripe.invoices.retrieve(invoiceId);
 
-        const transporter=nodemailer.createTransport({
-          service:'gmail',
-          auth:{
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASS
-           },
-        });
+        // const transporter=nodemailer.createTransport({
+        //   service:'gmail',
+        //   auth:{
+        //     user:process.env.EMAIL_USER,
+        //     pass:process.env.EMAIL_PASS
+        //    },
+        // });
 
-        //Sending the mail with subscription details and invoice
-        const mailOptions={
-          from:process.env.EMAIL_USER,
-          to:email,
-          subject:'Subscription Confirmation and Invoice',
-          text:`Thank you for subscribing to our plan.Here are your details:\n\n
-          Plan:${plan}\n
-          Amount Paid:${invoice.total/100} ${invoice.currency.toUpperCase()}\n
-          Invoice URL:${invoice.hosted_inovice_url}\n\n
-          Thank you..`,
-        };
+        // //Sending the mail with subscription details and invoice
+        // const mailOptions={
+        //   from:process.env.EMAIL_USER,
+        //   to:email,
+        //   subject:'Subscription Confirmation and Invoice',
+        //   text:`Thank you for subscribing to our plan.Here are your details:\n\n
+        //   Plan:${plan}\n
+        //   Amount Paid:${invoice.total/100} ${invoice.currency.toUpperCase()}\n
+        //   Invoice URL:${invoice.hosted_inovice_url}\n\n
+        //   Thank you..`,
+        // };
 
         await transporter.sendMail(mailOptions);
 
