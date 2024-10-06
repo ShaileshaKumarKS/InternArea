bb7fb9a1c2cac6701064324fac644008df4de2ec
ShaileshaKumarKS <shailesh4ks@gmail.com>
2024-10-06 16:52:41 +0530

    Added stripe payment feature

--- Routes/Payment.js
+++ Routes/Payment.js
@@ -26,31 +26,31 @@ router.post('/', async (req, res) => {
           
         });
 
-        // const invoiceId = subscription.latest_invoice;
+        const invoiceId = subscription.latest_invoice;
         
-        // const invoice=await stripe.invoices.retrieve(invoiceId);
+        const invoice=await stripe.invoices.retrieve(invoiceId);
 
-        // const transporter=nodemailer.createTransport({
-        //   service:'gmail',
-        //   auth:{
-        //     user:process.env.EMAIL_USER,
-        //     pass:process.env.EMAIL_PASS,
-        //    },
-        // });
+        const transporter=nodemailer.createTransport({
+          service:'gmail',
+          auth:{
+            user:process.env.EMAIL_USER,
+            pass:process.env.EMAIL_PASS,
+           },
+        });
 
-        // //Sending the mail with subscription details and invoice
-        // const mailOptions={
-        //   from:process.env.EMAIL_USER,
-        //   to:email,
-        //   subject:'Subscription Confirmation and Invoice',
-        //   text:`Thank you for subscribing to our plan.Here are your details:\n\n
-        //   Plan:${plan}\n
-        //   Amount Paid:${invoice.total/100} ${invoice.currency.toUpperCase()}\n
-        //   Invoice URL:${invoice.hosted_inovice_url}\n\n
-        //   Thank you..`,
-        // };
+        //Sending the mail with subscription details and invoice
+        const mailOptions={
+          from:process.env.EMAIL_USER,
+          to:email,
+          subject:'Subscription Confirmation and Invoice',
+          text:`Thank you for subscribing to our plan.Here are your details:\n\n
+          Plan:${plan}\n
+          Amount Paid:${invoice.total/100} ${invoice.currency.toUpperCase()}\n
+          Invoice URL:${invoice.hosted_inovice_url}\n\n
+          Thank you..`,
+        };
 
-        // await transporter.sendMail(mailOptions);
+        await transporter.sendMail(mailOptions);
 
         res.status(200).json(subscription); // Send back the subscription data
     } catch (error) {
